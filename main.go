ea4c2f102ad915b1ffe68d020806fe241b0bb841
Ilya <youn.ga@yandex.ru>
2026-09-06 15:49:49 +0300

    first-commit

--- main.go
+++ main.go
@@ -98,8 +98,13 @@ func (s ParcelService) Delete(number int) error {
 
 func main() {
 	// настройте подключение к БД
+	db, err := sql.Open("sqlite", "tracker.db")
+	if err != nil {
+		fmt.Printf("Ошибка открытия файла tracker.db: %s", err)
+	}
+	defer db.Close()
 
-	store := // создайте объект ParcelStore функцией NewParcelStore
+	store := NewParcelStore(db) // создайте объект ParcelStore функцией NewParcelStore
 	service := NewParcelService(store)
 
 	// регистрация посылки
